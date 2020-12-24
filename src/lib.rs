abc50240b9409d0ff1fb32e92d2d14dd9dd16743
Eduardo Tongson <propolice@gmail.com>
2020-12-24 11:07:48 +0800

    src: Actual initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::ffi::CStr;
+use std::os::raw::{c_char, c_uint};
+use std::panic;
+
+extern crate serde_json;
+extern crate zxcvbn;
+use serde::Deserialize;
+use serde_json::from_slice;
+use zxcvbn::zxcvbn;
+
+#[no_mangle]
+pub extern "C" fn score(c: *const c_char) -> c_uint {
+  panic::set_hook(Box::new(move |_| eprintln!("panic: passwdqc.check()")));
+  #[derive(Deserialize)]
+  struct Args {
+    password: String,
+    inputs: Vec<String>,
+  }
+  let cb = unsafe { CStr::from_ptr(c).to_bytes() };
+  let v: Args = match from_slice(cb) {
+    Ok(jv) => jv,
+    Err(_) => return 5,
+  };
+  let i: Vec<_> = v.inputs.iter().map(String::as_str).collect();
+  let _: () = match zxcvbn(&v.password, &i) {
+    Ok(entropy) => return entropy.score().into(),
+    Err(_) => return 5,
+  };
+}
